1fa530ef8078d7ec68013d3a0e487d47df36976a
Jarek Lipski <loomchild@loomchild.net>
2020-05-23 21:39:18 +0200

    initial commit based on bg color

---
+++ README.md
@@ -0,0 +1,22 @@
+# sticky-vuetify-table-header
+
+> A project demonstrating how to add sticky data table headers in Vuetify.js and Nuxt.js 
+
+Read my related article on [Medium](...) to learn more.
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+export default {
+  mode: 'spa',
+
+  head: {
+    title: 'Sticky table headers',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Sticky table headers description.' }
+    ],
+    link: [
+    ]
+  },
+
+  loading: { color: '#f00' },
+
+  css: [
+  ],
+
+  plugins: [
+  ],
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+
+  modules: [
+  ],
+
+  vuetify: {
+  }
+}
