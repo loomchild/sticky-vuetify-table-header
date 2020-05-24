2ee116d4de24ffc4d8fa6ac94bab5be14148aa04
Jarek Lipski <loomchild@loomchild.net>
2020-05-24 14:52:18 +0200

    allow more attribs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
   root: true,
+
   env: {
     browser: true,
     node: true
   },
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
-  // add your custom rules here
+
   rules: {
+    'vue/max-attributes-per-line': [
+      'error', {
+        singleline: 16
+      }
+    ]
   }
 }
